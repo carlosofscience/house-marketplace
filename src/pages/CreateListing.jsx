846c7b9bbd550ad5a6a4af6298540cced22c902b
carlosofscience <carlosofscience@gmail.com>
2022-12-11 01:59:37 -0500

    saving images to firebase storage

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect, useRef } from "react";
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { db } from '../firebase.config'
 import { useNavigate } from 'react-router-dom'
 import Spinner from '../components/Spinner'
 import { toast } from "react-toastify";
+import { v4 as uuidv4 } from 'uuid'
 
 function CreateListing() {
   const [geolocationEnabled, setGeolocationEnabled] = useState(true);
@@ -84,14 +87,15 @@ function CreateListing() {
         `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
       );
       const data = await response.json()
-      
+      console.log(data.results[0]);
       geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
       geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
       
-      location = data.status === "ZERO_RESULTS" ? undefined : data.results[0].formatted_address
-      
+      location = data.status === "ZERO_RESULTS" ? undefined : data.results[0]?.formatted_address
+
       if (location === undefined || location.includes('undefined')){
         setLoading(false)
+        
         toast.error('Please enter correct address')
         return
       }
@@ -104,6 +108,52 @@ function CreateListing() {
       console.log(geolocation);
       console.log(location);
     }
+
+    //Store Images in firebase
+    const storeImage = async (image) =>{
+      return new Promise((resolve, reject)=>{
+        const storage = getStorage()
+        const filename = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+        const storageRef = ref(storage, 'images/'+filename)
+        const uploadTask = uploadBytesResumable(storageRef, image)
+        
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+            }
+          },
+          (error) => {
+            reject(error)
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              console.log("File available at", downloadURL);
+              resolve(downloadURL)
+            });
+          }
+        );
+      })
+    }
+
+    const imgUrls = await Promise.all(
+      [...images].map((image)=>(storeImage(image)))
+    ).catch(()=>{
+      setLoading(false)
+      toast.error('Images not uploaded')
+      return
+    })
+
+    console.log(imgUrls);
 
     setLoading(false)
   };
