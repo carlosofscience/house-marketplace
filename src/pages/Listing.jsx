2c498857d88bf53c86064eaeb62bd8fc6a530176
carlosofscience <carlosofscience@gmail.com>
2022-12-12 00:25:31 -0500

    adding recommened listings in explore + slides

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -19,6 +19,7 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 // import Swiper core and required modules
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css'
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -68,24 +69,6 @@ function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
-
-      {/* <Swiper
-        // install Swiper modules
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={50}
-        slidesPerView={3}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log("slide change")}
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-      </Swiper> */}
-
       <div
         className="shareIconDiv"
         onClick={() => {
